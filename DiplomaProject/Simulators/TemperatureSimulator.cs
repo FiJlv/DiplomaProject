d79c3b8460683d28bc6f3a239df324009cd88454
Vladyslav Filatov <vladzzzzxd@gmail.com>
2023-03-25 13:11:36 +0200

    front refactoring

---
+++ DiplomaProject/Simulators/TemperatureSimulator.cs
@@ -0,0 +1,22 @@
+﻿using DiplomaProject.Models;
+using System.Collections.Generic;
+
+namespace DiplomaProject.Simulators
+{
+    public static class TemperatureSimulator
+    { 
+        //public static List<Temperature> Generate()
+        //{
+        //    Random random = new Random();
+
+        //    List<Temperature> values = new List<Temperature>(8);
+
+        //    for(int i = 0; i < values.Count; i++) 
+        //    {
+        //        values[i].Value = random.Next(30,45);
+        //    }
+
+        //    return values;
+        //}
+    }
+}
